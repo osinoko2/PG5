0584c13446e8b04a4a2fe7573e9df7123661f7df
osinoko <k023g0114@g.neec.ac.jp>
2025-04-16 11:16:27 +0900

    KamataEngine前準備

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,5 +1,7 @@
-#include <Windows.h>
 #include "KamataEngine.h"
+#include <Windows.h>
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -7,12 +9,21 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// エンジンの初期化
 	KamataEngine::Initialize(L"LE3C_11_サクライ_ショウセイ");
 
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
 	// メインループ
 	while (true) {
 		// エンジンの更新
 		if (KamataEngine::Update()) {
 			break;
 		}
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// 描画終了
+		dxCommon->PostDraw();
 	}
 
 	// エンジンの終了処理
